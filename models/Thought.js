b7276e5f19ea30677dcb7af14f0314dbd1bf38b6
Kar Sodhi <karsodhi1212@gmail.com>
2023-07-06 16:35:58 -0500

    updated files

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
 
 const thoughtSchema = new Schema(
   {
